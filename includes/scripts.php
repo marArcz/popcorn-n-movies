11fc5c8779dfe4a2e5ac2406bc33c44e1bd0ed21
marArcz <mar19arcz@gmail.com>
2024-08-16 13:44:50 +0800

    added adsterra ads: Banner 320x50

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,4 +1,14 @@
 <script type='text/javascript' src='//greetingsdaydreamlitre.com/60/09/94/6009940b692721f27d0d18ce4b07ba69.js'></script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '68594368700ac291d72072bd9fff00f3',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//greetingsdaydreamlitre.com/68594368700ac291d72072bd9fff00f3/invoke.js"></script>
 <script src="../assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     document.addEventListener("DOMContentLoaded", function() {
