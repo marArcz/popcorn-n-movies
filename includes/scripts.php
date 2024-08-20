782293356e34dd757b58452abd81ca29240e9862
marArcz <mar19arcz@gmail.com>
2024-08-20 11:16:26 +0800

    added jquery

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,23 +1,25 @@
 <script type='text/javascript' src='//greetingsdaydreamlitre.com/60/09/94/6009940b692721f27d0d18ce4b07ba69.js'></script>
 <script type="text/javascript">
 	atOptions = {
-		'key' : '68594368700ac291d72072bd9fff00f3',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
+		'key': '68594368700ac291d72072bd9fff00f3',
+		'format': 'iframe',
+		'height': 50,
+		'width': 320,
+		'params': {}
 	};
 </script>
 <script type="text/javascript" src="//greetingsdaydreamlitre.com/68594368700ac291d72072bd9fff00f3/invoke.js"></script>
 
 <script type="text/javascript">
 	atOptions = {
-		'key' : 'bb6d76478a3c0368948ae3c45c57d4d5',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
+		'key': 'bb6d76478a3c0368948ae3c45c57d4d5',
+		'format': 'iframe',
+		'height': 250,
+		'width': 300,
+		'params': {}
 	};
 </script>
 <script type="text/javascript" src="//greetingsdaydreamlitre.com/bb6d76478a3c0368948ae3c45c57d4d5/invoke.js"></script>
 <script src="../assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/jquery-3.7.1.min.js"></script>
+<script src="../assets/js/main.js"></script>
